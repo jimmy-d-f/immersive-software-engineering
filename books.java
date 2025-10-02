2540735bcdf39fd702884e9173de76bae94ddeff
Jimmy F. <jimmyfrosting@gmail.com>
2025-10-02 16:01:48 +0100

    Update books.java

--- books.java
+++ books.java
@@ -13,73 +13,82 @@ public class books
         Scanner scanner = new Scanner(System.in);
         int choice;
 
-        do
+
+        boolean wahl = true;
+        while (true)
         {
-            System.out.println("\nLibrary Menu:");
-            System.out.println("\n\n1. Display all books");
-            System.out.println("2. Add a book");
-            System.out.println("3. Delete a book by ISBN number");
-            System.out.println("4. Exit");
-            System.out.print("\nEnter your choice: ");
-
-            choice = scanner.nextInt();
-
-            scanner.nextLine();
-
-            switch (choice)
+            do
             {
-                case 1:
-                    library.displayLibraryBooks();
-                    break;
+                System.out.println("\nLibrary Menu:");
+                System.out.println("\n\n1. Display all books");
+                System.out.println("2. Add a book");
+                System.out.println("3. Delete a book by ISBN number");
+                System.out.println("4. Exit");
+                System.out.print("\nEnter your choice: ");
 
-                case 2:
-                    System.out.print("\nEnter Author: ");
-                    String author = scanner.nextLine();
+                choice = scanner.nextInt();
 
-                    System.out.print("Enter Title: ");
-                    String title = scanner.nextLine();
+                scanner.nextLine();
 
-                    System.out.print("Enter year: ");
-                    int madeIn = scanner.nextInt();
+                switch (choice)
+                {
+                    case 1:
+                        library.displayLibraryBooks();
+                        wahl = false;
+                        break;
 
-                    System.out.print("Enter ISBN number: ");
-                    int ISBN = scanner.nextInt();
-                    scanner.nextLine();
+                    case 2:
+                        System.out.print("\nEnter Author: ");
+                        String author = scanner.nextLine();
 
-                    Book newBook = new Book(author, title, madeIn, ISBN);
-                    library.addBook(newBook);
-                    System.out.println("\nBook added.");
-                    break;
+                        System.out.print("Enter Title: ");
+                        String title = scanner.nextLine();
 
-                case 3:
-                    System.out.print("Enter ISBN number to delete: ");
-                    int isbnToDelete = scanner.nextInt();
+                        System.out.print("Enter year: ");
+                        int madeIn = scanner.nextInt();
 
-                    scanner.nextLine();
+                        System.out.print("Enter ISBN number: ");
+                        int ISBN = scanner.nextInt();
+                        scanner.nextLine();
 
-                    if (library.deleteBook(isbnToDelete))
-                    {
-                        System.out.println("Book deleted.");
-                    }
+                        Book newBook = new Book(author, title, madeIn, ISBN);
+                        library.addBook(newBook);
+                        System.out.println("\nBook added.");
+                        wahl = false;
+                        break;
 
-                    else
-                    {
-                        System.out.println("Book not found.");
-                    }
+                    case 3:
+                        System.out.print("Enter ISBN number to delete: ");
+                        int isbnToDelete = scanner.nextInt();
 
-                    break;
+                        scanner.nextLine();
 
-                case 4:
-                    System.out.println("Exiting program.");
-                    break;
+                        if (library.deleteBook(isbnToDelete))
+                        {
+                            System.out.println("Book deleted.");
+                        }
 
-                default:
-                    System.out.println("Invalid choice.");
+                        else
+                        {
+                            System.out.println("Book not found.");
+                        }
+
+                        wahl = false;
+                        break;
+
+                    case 4:
+                        System.out.println("Exiting program.");
+                        wahl = true;
+                        break;
+
+                    default:
+                        System.out.println("Invalid choice.");
+                        wahl = true;
+                }
             }
-        }
 
-        while (choice != 4);
-        scanner.close();
+            while (choice != 4);
+        }
     }
 }
 
@@ -162,8 +171,9 @@ class Book
     {
         System.out.println
         ("Author is: " + author +
-        ", title is: " + title +
-        ", made in the year: " + madeIn +
-        ", ISBN is: " + ISBN);
+        ", \nTitle is: " + title +
+        ", \nMade in the year: " + madeIn +
+        ", \nISBN is: " + ISBN);
+        System.out.println();
     }
 }
