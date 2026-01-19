b92dcac212ff663d1abfb7c5d5283efe57ed16df
jimmy <jimmyfrosting@gmail.com>
2026-01-19 10:00:56 +0000

    added morning files

--- James Patten/Months.java
+++ James Patten/Months.java
@@ -2,31 +2,31 @@ public class Months
 {
     public static void main(String[] args) 
     {
-        double[][] months = new double[12][];
+        int[][] months = new int[12][];
 
-        months[0] = new double[] {31}; 
-        months[1] = new double[] {28}; 
-        months[2] = new double[] {31}; 
-        months[3] = new double[] {30}; 
-        months[4] = new double[] {31}; 
-        months[5] = new double[] {30};
-        months[6] = new double[] {31};
-        months[7] = new double[] {31}; 
-        months[8] = new double[] {30}; 
-        months[9] = new double[] {31}; 
-        months[10] = new double[] {30}; 
-        months[11] = new double[] {31}; 
-    
+        months[0] = new int[] {31}; 
+        months[1] = new int[] {28}; 
+        months[2] = new int[] {31}; 
+        months[3] = new int[] {30}; 
+        months[4] = new int[] {31}; 
+        months[5] = new int[] {30};
+        months[6] = new int[] {31};
+        months[7] = new int[] {31}; 
+        months[8] = new int[] {30}; 
+        months[9] = new int[] {31}; 
+        months[10] = new int[] {30}; 
+        months[11] = new int[] {31}; 
+
         String[] monthNames = {
             "January", "February", "March", "April", "May", "June",
             "July", "August", "September", "October", "November", "December"
         };
 
-        System.out.printf("%-10s %s%n", "Month", "Days");
+        System.out.printf("%-10s %s\n", "Month", "Days");
         System.out.println("-------------------");
         for (int i = 0; i < months.length; i++) 
         {
-            System.out.printf("%-10s %.0f%n", monthNames[i], months[i][0]);
+            System.out.printf("%-10s %d\n", monthNames[i], months[i][0]);
         }
     }
 }
